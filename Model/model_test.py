0d9ed56922f6b4b760319155c1a4ee40ed93abca
jiminAn <54790133+jiminAn@users.noreply.github.com>
2020-08-22 17:36:58 +0900

    Update model_test.py

--- Model/model_test.py
+++ Model/model_test.py
@@ -6,6 +6,17 @@ import shutil
 from numpy import argmax
 from keras.models import load_model
  
+ 
+#
+# filename : cnn.py
+# history
+# =============================
+# 20200818 v.1.0 초안 작성 안지민
+# 20200822 v.1.1 Dataization 반환값 1차원으로 수정 : (28,28,3) -> (28,28,1) : test set랑 사이즈 맞추기
+# =============================
+# Ver 1.1
+
+
 #분류할 카테고리 지정: 단, read_img.py에서와 카테고리 분류 이름 동일해야함 
 categories = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
               "A", "C", "D", "E", "F", "H", "J","K", "L", "M",
@@ -15,8 +26,11 @@ def Dataization(img_path):
     image_w = 28
     image_h = 28
     img = cv2.imread(img_path)
-    img = cv2.resize(img, None, fx=image_w/img.shape[1], fy=image_h/img.shape[0])
-    return (img/256)
+    img = cv2.resize(img, None, fx=image_w / img.shape[1], fy=image_h / img.shape[0])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #1차원으로 수정하기 
+    temp_img = img / 256
+    temp_img = temp_img.reshape(28, 28, 1)  
+    return (temp_img / 256)
  
 src = []
 name = []
@@ -30,7 +44,7 @@ for file in os.listdir(image_dir):
  
  
 test = np.array(test)
-model = load_model('Gersang.h5') #사용할 모델 불러오기
+model = load_model('hand_writing.h5') #사용할 모델 불러오기
 predict = model.predict_classes(test)
  
 for i in range(len(test)): # 알파벳 예측 결과 출력
