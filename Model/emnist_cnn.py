0b292a869e1c20bc25b367c4526dffe5681f1236
jiminAn <54790133+jiminAn@users.noreply.github.com>
2020-08-26 15:43:16 +0900

    mnist/emnist_cnn_code

---
+++ Model/emnist_cnn.py
@@ -0,0 +1,86 @@
+# Visualization Dependencies
+from IPython.display import Image, SVG
+import seaborn as sns
+
+# Filepaths, Numpy, Tensorflow
+import os
+import numpy as np
+import tensorflow as tf
+
+# Keras
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras import backend as K
+from tensorflow.keras.datasets import mnist
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+#ignore warning messages
+import warnings
+warnings.filterwarnings('ignore')
+
+sns.set()
+
+
+# pip install emnist
+# Import Dataset(s)
+from emnist import list_datasets
+list_datasets()
+
+from emnist import extract_training_samples
+images_train, labels_train = extract_training_samples('letters')
+from emnist import extract_test_samples
+images_test, labels_test = extract_test_samples('letters')
+
+# Flatten Data
+dims = images_train.shape[1] * images_train.shape[2]
+X_train = images_train.reshape(images_train.shape[0], dims)
+X_test = images_test.reshape(images_test.shape[0], dims)
+
+
+# Rescale to 0 -> 1 by dividing by max pixel value (255)
+X_train = X_train.astype('float32')/255
+X_test = X_test.astype('float32')/255
+
+
+# One-Hot Encoding
+
+from keras.utils import np_utils # used to convert array of labeled data to one-hot vector
+# should be 26 but out of index?
+# Effects accuracy as have a class where their will be no results
+num_classes = 27
+y_train = np_utils.to_categorical(labels_train, num_classes)
+y_test = np_utils.to_categorical(labels_test, num_classes)
+
+
+# Empty Sequential model
+from tensorflow.keras.models import Sequential
+model = Sequential()
+
+#Layers
+
+# 1 - number of elements (pixels) in each image
+# Dense layer - when every node from previous layer is connected to each node in current layer
+model.add(Dense(500, activation='relu'))
+
+# Second Hidden Layer
+model.add(Dense(500, activation='relu'))
+
+# Output Layer - number of nodes corresponds to number of y labels
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile Model
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop' , metrics=['accuracy'])
+
+
+# Train Model
+model.fit(X_train, y_train, batch_size=128, epochs=10, shuffle=True, verbose=2)
+
+model.summary()
+
+# Save Model
+model.save("emnist_trained.h5")
---
+++ Model/mnist_my_app.py
@@ -0,0 +1,112 @@
+## Ex 10-6. MNIST 손글씨 인식 프로그램.
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import numpy as np
+import tensorflow as tf
+
+
+class MyApp(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.image = QImage(QSize(400, 400), QImage.Format_RGB32)
+        self.image.fill(Qt.white)
+        self.drawing = False
+        self.brush_size = 30
+        self.brush_color = Qt.black
+        self.last_point = QPoint()
+        self.loaded_model = None
+        self.initUI()
+
+    def initUI(self):
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('File')
+
+        load_model_action = QAction('Load model', self)
+        load_model_action.setShortcut('Ctrl+L')
+        load_model_action.triggered.connect(self.load_model)
+
+        save_action = QAction('Save', self)
+        save_action.setShortcut('Ctrl+S')
+        save_action.triggered.connect(self.save)
+
+        clear_action = QAction('Clear', self)
+        clear_action.setShortcut('Ctrl+C')
+        clear_action.triggered.connect(self.clear)
+
+        filemenu.addAction(load_model_action)
+        filemenu.addAction(save_action)
+        filemenu.addAction(clear_action)
+
+        self.statusbar = self.statusBar()
+
+        self.setWindowTitle('MNIST Classifier')
+        self.setGeometry(300, 300, 400, 400)
+        self.show()
+
+    def paintEvent(self, e):
+        canvas = QPainter(self)
+        canvas.drawImage(self.rect(), self.image, self.image.rect())
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            self.drawing = True
+            self.last_point = e.pos()
+
+    def mouseMoveEvent(self, e):
+        if (e.buttons() & Qt.LeftButton) & self.drawing:
+            painter = QPainter(self.image)
+            painter.setPen(QPen(self.brush_color, self.brush_size, Qt.SolidLine, Qt.RoundCap))
+            painter.drawLine(self.last_point, e.pos())
+            self.last_point = e.pos()
+            self.update()
+
+    def mouseReleaseEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            self.drawing = False
+
+            arr = np.zeros((28, 28))
+            for i in range(28):
+                for j in range(28):
+                    arr[j, i] = 1 - self.image.scaled(28, 28).pixelColor(i, j).getRgb()[0] / 255.0
+            arr = arr.reshape(-1,28,28)
+
+
+            if self.loaded_model:
+                #mnist_trained.h5쓸거면 아래
+                arr = arr.flatten().reshape(1, 28,28)
+                #model.h5쓸꺼면 위에꺼 주석처리하고 아래
+                #arr = arr.flatten().reshape(1, 1, 28, 28)
+                pred = self.loaded_model.predict(arr)[0]
+                pred_num = str(np.argmax(pred))
+                self.statusbar.showMessage('숫자 ' + pred_num + '입니다.')
+
+    def load_model(self):
+        fname, _ = QFileDialog.getOpenFileName(self, 'Load Model', './model1.h5')
+
+
+        if fname:
+            self.loaded_model = tf.keras.models.load_model(fname)
+            self.statusbar.showMessage('Model loaded.')
+
+    def save(self):
+        fpath, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
+
+        if fpath:
+            self.image.scaled(28, 28).save(fpath)
+
+    def clear(self):
+        self.image.fill(Qt.white)
+        self.update()
+        self.statusbar.clearMessage()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    ex.load_model()
+    sys.exit(app.exec_())
